54224339969665c9b5bc6f3eff787be17675cbd9
Engineer Fred <engfred88@gmail.com>
2026-05-13 09:06:34 +0300

    updated

--- src/modules/properties/properties.module.ts
+++ src/modules/properties/properties.module.ts
@@ -1,19 +1,25 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Property } from './entities/property.entity';
-import { PropertyImage } from './entities/property-image.entity';
-import { PropertiesService } from './properties.service';
-import { PropertiesController } from './properties.controller';
-import { DistrictsModule } from '../districts/districts.module';
-import { ContactsModule } from '../contacts/contacts.module';
+import { Property }                from './entities/property.entity';
+import { PropertyImage }           from './entities/property-image.entity';
+import { PropertiesService }       from './properties.service';
+import { PropertiesController }    from './properties.controller';
+import { PropertiesSchedulerTask } from './tasks/properties-scheduler.task';
+import { DistrictsModule }         from '../districts/districts.module';
+import { ContactsModule }          from '../contacts/contacts.module';
+import { UniversitiesModule }      from '../universities/universities.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Property, PropertyImage]),
     ContactsModule,
     DistrictsModule,
+    UniversitiesModule, // provides UniversitiesService for FK resolution + seeding
   ],
-  providers: [PropertiesService],
+  providers: [
+    PropertiesService,
+    PropertiesSchedulerTask, // nightly cron — expires featured listings automatically
+  ],
   controllers: [PropertiesController],
   exports: [PropertiesService],
 })
